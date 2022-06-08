db39f7c10b2bb0de166932597e8886674ee84ecc
Stéphane Francel <stephane.francel@gmail.com>
2022-06-08 07:40:18 -0700

    Add product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, ProductComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from './product.types';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+  @Input()
+  product!: Product;
+
+  @Output()
+  addToBasket = new EventEmitter<number>();
+
+  constructor() { }
+
+  triggerAddToBasket() {
+    this.addToBasket.emit(this.product.price);
+  }
+}
