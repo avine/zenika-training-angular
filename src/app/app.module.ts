8ee1f8085ecd57f50c13013dfe6fbf506f6832ef
Stéphane Francel <stephane.francel@gmail.com>
2022-10-19 15:23:03 +0000

    Add menu and product components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent, ProductComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
