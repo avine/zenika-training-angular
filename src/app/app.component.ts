b06b0682bf38ce7966c6791e86fdce41f048548a
Stéphane Francel <stephane.francel@gmail.com>
2022-10-20 09:18:09 +0000

    Use ngFor, ngIf and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,47 @@ import { Product } from './product/product.types';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'shop';
+  protected title = 'shop';
 
-  total = 0;
-
-  products: Product[] = [
+  protected products: Product[] = [
     {
       title: 'Sweat homme',
       description: '<C0D1NG_TH3_W0RLD> SWEATSHIRT CHAUD BIO À CAPUCHE - HOMME',
       photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
       price: 39,
+      stock: 3,
     },
     {
       title: 'Tee-Shirt homme',
       description: 'TEE-SHIRT BIO À COL ROND - HOMME',
       photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b2911e4ab33424aec592bd6/Mockups/front.png',
       price: 19,
+      stock: 2,
     },
     {
       title: 'Tee-Shirt femme',
       description: 'TEE-SHIRT BIO À COL ROND - FEMME',
       photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b290d26ab33424aec592bd4/Mockups/front.png',
       price: 19,
+      stock: 2,
     },
     {
       title: 'Tote bag',
       description: '<C0D1NG_TH3_W0RLD>, TOTE BAG BIO.',
       photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf160814006a7fe670e2dd/Mockups/front.png',
       price: 12.5,
+      stock: 2,
     },
   ];
 
-  addToBasket(product: Product) {
-    this.total += product.price;
+  protected basket: Product[] = [];
+
+  get total() {
+    return this.basket.reduce((total, product) => total + product.price, 0);
+  }
+
+  protected addToBasket(product: Product) {
+    product.stock--;
+    this.basket.push(product);
   }
 }
