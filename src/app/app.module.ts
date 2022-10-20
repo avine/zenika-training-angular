8aa8f63a47881d11dcfd71d3a9c63c1bacc11cff
Stéphane Francel <stephane.francel@gmail.com>
2022-10-20 22:26:42 +0000

    Add localize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { APP_NAME } from './app.token';
@@ -10,7 +10,16 @@ import { ArraySorterPipe } from './shared/array-sorter.pipe';
 @NgModule({
   declarations: [AppComponent, MenuComponent, ProductComponent, ArraySorterPipe],
   imports: [BrowserModule, HttpClientModule],
-  providers: [{ provide: APP_NAME, useValue: 'Welcome to Zenika Ecommerces' }],
+  providers: [
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'EUR',
+    },
+    {
+      provide: APP_NAME,
+      useValue: 'Welcome to Zenika Ecommerces',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
